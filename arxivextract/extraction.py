35f4d31d308bf671c92a8b425b825c030d18b211
kwanyuetho <kwanyuetho@gmail.com>
2025-03-19 21:27:28 +0000

    more implementation

--- arxivextract/extraction.py
+++ arxivextract/extraction.py
@@ -1,4 +1,26 @@
 
+import urllib.request
+
+
+base_url = 'http://export.arxiv.org/api/query?'
+
+
 
 class ArXivExtractor:
-    pass
+    def retrieve_articles(date, start=0, max_results=100):
+        query = f'search_query=submittedDate:[{date}0000+TO+{date}2359]&start={start}&max_results={max_results}&sortBy=submittedDate&sortOrder=ascending'
+
+        url = base_url + query
+
+        response = urllib.request.urlopen(url).read()
+        feed = feedparser.parse(response)
+
+        for entry in feed.entries:
+            article = {
+                'title': entry.title,
+                'authors': [author.name for author in entry.authors],
+                'published': entry.published,
+                'summary': entry.summary,
+                'arxiv_url': entry.link
+            }
+            
