2a5206b8bef38d3e353a889243c4232fc440b0f2
Kwan Yuet Stephen Ho <3810067+stephenhky@users.noreply.github.com>
2025-03-22 11:27:54 -0400

    extractor working

--- arxivextract/extraction.py
+++ arxivextract/extraction.py
@@ -1,20 +1,27 @@
 
 import urllib.request
 
+import feedparser
+
 
 base_url = 'http://export.arxiv.org/api/query?'
 
 
 
 class ArXivExtractor:
-    def retrieve_articles(date, start=0, max_results=100):
-        query = f'search_query=submittedDate:[{date}0000+TO+{date}2359]&start={start}&max_results={max_results}&sortBy=submittedDate&sortOrder=ascending'
+    def retrieve_articles(self, dts, start=0, max_results=100):
+        query = 'search_query=submittedDate:[{dts:}0000+TO+{dts:}2359]&start={start:}&max_results={max_results:}&sortBy=submittedDate&sortOrder=ascending'.format(
+            dts=dts,
+            start=start,
+            max_results=max_results
+        )
 
         url = base_url + query
 
         response = urllib.request.urlopen(url).read()
         feed = feedparser.parse(response)
 
+        articles = []
         for entry in feed.entries:
             article = {
                 'title': entry.title,
@@ -23,4 +30,7 @@ class ArXivExtractor:
                 'summary': entry.summary,
                 'arxiv_url': entry.link
             }
+            articles.append(article)
+
+        return articles
             
