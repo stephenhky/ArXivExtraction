30bc47bd2633f459051f0852b1943d1a89914448
stephenhky <stephenhky@yahoo.com.hk>
2025-05-07 13:54:33 -0400

    typo

--- arxivextract/keywordutils/yake.py
+++ arxivextract/keywordutils/yake.py
@@ -10,7 +10,7 @@ class YakeKeywordExtractor(AbstractKeywordExtractor):
     def __init__(self):
         self._yake_extractor = yake.KeywordExtractor()
 
-    def _get_keyword_extraction_info(self, text: str) -> list(Tuple[str, float]):
+    def _get_keyword_extraction_info(self, text: str) -> list[Tuple[str, float]]:
         return self._yake_extractor.extract_keywords(text)
 
     def extract_keywords(self, text: str) -> list[str]:
