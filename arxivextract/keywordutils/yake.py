8325d6599c3f6ade4e897961464a37003da849ff
stephenhky <stephenhky@yahoo.com.hk>
2025-05-07 10:06:05 -0400

    added yake extractor

---
+++ arxivextract/keywordutils/yake.py
@@ -0,0 +1,18 @@
+
+from typing import Tuple
+
+import yake
+
+from ..keywords import AbstractKeywordExtractor
+
+
+class YakeKeywordExtractor(AbstractKeywordExtractor):
+    def __init__(self):
+        self._yake_extractor = yake.KeywordExtractor()
+
+    def _get_keyword_extraction_info(self, text: str) -> list(Tuple[str, float]):
+        return self._yake_extractor.extract_keywords(text)
+
+    def extract_keywords(self, text: str) -> list[str]:
+        info = self._get_keyword_extraction_info(text)
+        return [keyword for keyword, _ in info]
