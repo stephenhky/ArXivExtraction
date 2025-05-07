b54598a62467470b6ec46a8a54d3c0a4b676c12d
stephenhky <stephenhky@yahoo.com.hk>
2025-05-07 14:14:28 -0400

    chopped down the article extraction

--- test/test_keywords.py
+++ test/test_keywords.py
@@ -10,13 +10,13 @@ from arxivextract.keywordutils.yake import YakeKeywordExtractor
 
 class TestKeywordExtraction(unittest.TestCase):
     def setUp(self):
-        self.arxiv_extractor = ArXivExtractor(nb_articles_each_turn=100)
+        self.arxiv_extractor = ArXivExtractor(nb_articles_each_turn=2)
 
     def test_keybert_1(self):
         keyword_extractor = make_keyword_extractor('2025-04-02-keyword')
         assert isinstance(keyword_extractor, KeywordBertKeywordExtractor)
 
-        all_article_entries = self.arxiv_extractor.retrieve_all_articles_given_date('20250327')
+        all_article_entries = self.arxiv_extractor._partial_retrieve_articles('20250327')
         assert isinstance(all_article_entries[0], BasicArticleEntry)
         all_article_entries = [
             ArticleEntryWithKeywords.make_entry_by_adding_keywords(entry, keyword_extractor)
@@ -28,7 +28,7 @@ class TestKeywordExtraction(unittest.TestCase):
         keyword_extractor = make_keyword_extractor('2025-04-23-keyword')
         assert isinstance(keyword_extractor, KeywordBertKeywordExtractor)
 
-        all_article_entries = self.arxiv_extractor.retrieve_all_articles_given_date('20250327')
+        all_article_entries = self.arxiv_extractor._partial_retrieve_articles('20250327')
         assert isinstance(all_article_entries[0], BasicArticleEntry)
         all_article_entries = [
             ArticleEntryWithKeywords.make_entry_by_adding_keywords(entry, keyword_extractor)
@@ -40,7 +40,7 @@ class TestKeywordExtraction(unittest.TestCase):
         keyword_extractor = make_keyword_extractor('2025-04-23-keyword')
         assert isinstance(keyword_extractor, YakeKeywordExtractor)
 
-        all_article_entries = self.arxiv_extractor.retrieve_all_articles_given_date('20250327')
+        all_article_entries = self.arxiv_extractor._partial_retrieve_articles('20250327')
         assert isinstance(all_article_entries[0], BasicArticleEntry)
         all_article_entries = [
             ArticleEntryWithKeywords.make_entry_by_adding_keywords(entry, keyword_extractor)
